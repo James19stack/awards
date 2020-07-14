143d1391015c0d9d782b0b634cf8259602e234dc
jefferson <mirarajames29@gmail.com>
2020-07-14 20:09:13 +0300

    Add the changes made in the email template

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Project,Votes,Profile
 
 # Register your models here.
+admin.site.site_header='AwwardMe Admin'
+admin.site.site_title='AwwardMe Admin Dashboard'
+
+
+class VotesInLine(admin.TabularInline):
+    model=Votes
+    extra=3
+    
+class ProjectInLine(admin.ModelAdmin):
+    fieldsets=[
+        (None,{'fields':['user']}),
+        (None,{'fields':['image']}),
+        (None,{'fields':['title']}),
+        (None,{'fields':['description']}),
+        (None,{'fields':['link']}),
+        (None,{'fields':['location']}),
+    ]
+    inlines=[VotesInLine]
+
+# Register your models here.
+admin.site.register(Project,ProjectInLine)
+admin.site.register(Profile)
